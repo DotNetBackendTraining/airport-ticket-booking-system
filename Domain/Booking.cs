f2e1929a29a9da5e4dcbce28818c8b311d00f259
Izzat Alsharif <work@izzatalsharif.com>
2024-03-01 22:39:52 +0200

    Add BookingClass property to `Booking`

--- Domain/Booking.cs
+++ Domain/Booking.cs
@@ -1,9 +1,10 @@
 namespace AirportTicketBookingSystem.Domain;
 
-public class Booking(Flight flight, Passenger passenger)
+public class Booking(Flight flight, Passenger passenger, FlightClass bookingClass)
 {
     public Flight Flight { get; } = flight;
     public Passenger Passenger { get; } = passenger;
+    public FlightClass BookingClass { get; set; } = bookingClass;
 
     public override bool Equals(object? obj) =>
         obj is Booking other &&
