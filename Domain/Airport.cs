851b6a431899765c6c19b1f5dbf9b980b9b17359
Izzat Alsharif <work@izzatalsharif.com>
2024-03-01 22:29:01 +0200

    Add string identifier to `Airport`

--- Domain/Airport.cs
+++ Domain/Airport.cs
@@ -1,15 +1,13 @@
 namespace AirportTicketBookingSystem.Domain;
 
-public class Airport(string name, string country)
+public class Airport(string id, string name, string country)
 {
-    public string Name { get; } = name;
-    public string Country { get; } = country;
+    public string Id { get; } = id;
+    public string Name { get; set; } = name;
+    public string Country { get; set; } = country;
 
     public override bool Equals(object? obj) =>
-        obj is Airport other &&
-        Name == other.Name &&
-        Country == other.Country;
+        obj is Airport other && Id == other.Id;
 
-    public override int GetHashCode() =>
-        HashCode.Combine(Name, Country);
+    public override int GetHashCode() => Id.GetHashCode();
 }
