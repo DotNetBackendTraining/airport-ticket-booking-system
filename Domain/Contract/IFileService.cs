c5a882be16f0f193a1e9a1df580da3b2c805590e
Izzat Alsharif <work@izzatalsharif.com>
2024-03-02 19:39:29 +0200

    Add `Replace` & `Remove` methods to `IFileService`

--- Domain/Contract/IFileService.cs
+++ Domain/Contract/IFileService.cs
@@ -4,4 +4,6 @@ public interface IFileService<T>
 {
     public IEnumerable<T> ReadAll();
     public Task AppendAsync(IEnumerable<T> entities);
+    public bool Replace(T oldEntity, T newEntity);
+    public bool Remove(T entity);
 }
