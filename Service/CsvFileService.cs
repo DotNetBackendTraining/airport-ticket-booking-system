d25ca3b2a9d376bb16e2f8946b715dda5ec7b7ad
Izzat Alsharif <work@izzatalsharif.com>
2024-03-02 17:41:59 +0200

    Implement `IFileService` contract in `CsvFileService`

--- Service/CsvFileService.cs
+++ Service/CsvFileService.cs
@@ -1,54 +1,28 @@
+using AirportTicketBookingSystem.Domain.Contract;
+
 namespace AirportTicketBookingSystem.Service;
 
-/// <summary>
-/// Provides static methods for reading from and writing to CSV files.
-/// </summary>
-public static class CsvFileService
+public class CsvFileService<T>(
+    string filepath,
+    ICsvEntityConverter<T> csvEntityConverter
+) : IFileService<T>
 {
-    /// <summary>
-    /// Reads data from a CSV file and converts each line into an instance of type <typeparamref name="T"/>.
-    /// </summary>
-    /// <param name="filepath">The path to the CSV file to be read.</param>
-    /// <param name="converter">A function that converts a single CSV line into an instance of <typeparamref name="T"/>.</param>
-    /// <param name="skipHeader">Specifies whether to skip the first line of the file, often used to ignore header rows. Defaults to false.</param>
-    /// <typeparam name="T">The type of object each line of the CSV file will be converted into.</typeparam>
-    /// <returns>An <see cref="IEnumerable{T}"/> where each element is an instance of <typeparamref name="T"/> representing a line from the CSV file.</returns>
-    /// <exception cref="FileNotFoundException">Thrown if the specified file does not exist.</exception>
-    public static IEnumerable<T> ReadCsvFile<T>(
-        string filepath, Func<string, T> converter, bool skipHeader = false)
+    private string Filepath { get; } = filepath;
+    private ICsvEntityConverter<T> Converter { get; } = csvEntityConverter;
+
+    public IEnumerable<T> ReadAll()
     {
-        if (!File.Exists(filepath))
-            throw new FileNotFoundException($"The file {filepath} was not found.");
+        if (!File.Exists(Filepath))
+            throw new FileNotFoundException($"The file {Filepath} was not found.");
 
-        using var reader = new StreamReader(filepath);
-        while (reader.ReadLine() is { } line)
-        {
-            if (skipHeader)
-            {
-                skipHeader = false;
-                continue;
-            }
-
-            var data = converter(line);
-            yield return data;
-        }
+        return File.ReadLines(Filepath)
+            .Skip(1) // header
+            .Select(line => Converter.CsvToEntity(line));
     }
 
-    /// <summary>
-    /// Writes a collection of data items to a CSV file, converting each item to a CSV line using the provided converter function.
-    /// </summary>
-    /// <param name="filepath">The path to the CSV file to be written.</param>
-    /// <param name="dataItems">An <see cref="IEnumerable{T}"/> of data items to be written to the file.</param>
-    /// <param name="converter">A function that converts an instance of <typeparamref name="T"/> into a CSV line.</param>
-    /// <typeparam name="T">The type of object that will be converted into a line for the CSV file.</typeparam>
-    public static void WriteCsvFile<T>(
-        string filepath, IEnumerable<T> dataItems, Func<T, string> converter)
+    public async Task AppendAsync(IEnumerable<T> entities)
     {
-        using var writer = new StreamWriter(filepath);
-        foreach (var data in dataItems)
-        {
-            var line = converter(data);
-            writer.WriteLine(line);
-        }
+        var csvLines = entities.Select(entity => Converter.EntityToCsv(entity));
+        await File.AppendAllLinesAsync(Filepath, csvLines);
     }
 }
