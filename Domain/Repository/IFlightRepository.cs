b7881ccaf2a1a1ecfeea450405f64d6be73ca9b1
Izzat Alsharif <work@izzatalsharif.com>
2024-03-02 18:05:11 +0200

    Add `AddAllAsync` method to `IFlightRepository` for async batch add operation

--- Domain/Repository/IFlightRepository.cs
+++ Domain/Repository/IFlightRepository.cs
@@ -6,6 +6,8 @@ public interface IFlightRepository
 {
     public void Add(Flight flight);
 
+    public Task AddAllAsync(IEnumerable<Flight> flights);
+
     public Flight GetById(int flightId);
 
     public IEnumerable<Flight> Search(FlightSearchCriteria criteria);
