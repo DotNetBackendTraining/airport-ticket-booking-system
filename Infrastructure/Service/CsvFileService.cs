1932e4e306c93aea54f6e063b6cbaa35361e8ba0
Izzat Alsharif <work@izzatalsharif.com>
2024-03-02 19:40:50 +0200

    Implement `Remove` & `Replace` using Caching in `CsvFileService` and hide the async implementation for now

--- Infrastructure/Service/CsvFileService.cs
+++ Infrastructure/Service/CsvFileService.cs
@@ -2,27 +2,72 @@ using AirportTicketBookingSystem.Domain.Contract;
 
 namespace AirportTicketBookingSystem.Infrastructure.Service;
 
-public class CsvFileService<T>(
-    string filepath,
-    ICsvEntityConverter<T> csvEntityConverter
-) : IFileService<T>
+public class CsvFileService<T> : IFileService<T>, IDisposable
 {
-    private string Filepath { get; } = filepath;
-    private ICsvEntityConverter<T> Converter { get; } = csvEntityConverter;
+    public CsvFileService(string filepath,
+        ICsvEntityConverter<T> csvEntityConverter)
+    {
+        Filepath = filepath;
+        Converter = csvEntityConverter;
+        Cache = LoadData();
+    }
 
-    public IEnumerable<T> ReadAll()
+    private string Filepath { get; }
+    private ICsvEntityConverter<T> Converter { get; }
+    private List<T> Cache { get; }
+    private bool CacheIsDirty { get; set; }
+
+    private List<T> LoadData()
     {
         if (!File.Exists(Filepath))
             throw new FileNotFoundException($"The file {Filepath} was not found.");
 
         return File.ReadLines(Filepath)
-            .Skip(1) // header
-            .Select(line => Converter.CsvToEntity(line));
+            .Skip(1) // Skip header
+            .Select(line => Converter.CsvToEntity(line))
+            .ToList();
     }
 
-    public async Task AppendAsync(IEnumerable<T> entities)
+    public IEnumerable<T> ReadAll() => Cache;
+
+    public Task AppendAsync(IEnumerable<T> entities)
     {
-        var csvLines = entities.Select(entity => Converter.EntityToCsv(entity));
-        await File.AppendAllLinesAsync(Filepath, csvLines);
+        Cache.AddRange(entities);
+        CacheIsDirty = true;
+        return Task.CompletedTask;
+    }
+
+    public bool Replace(T oldEntity, T newEntity)
+    {
+        var i = Cache.IndexOf(oldEntity);
+        if (i == -1) return false;
+        Cache[i] = newEntity;
+        return CacheIsDirty = true;
+    }
+
+    public bool Remove(T entity)
+    {
+        if (Cache.Remove(entity))
+            return CacheIsDirty = true;
+        return false;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing && CacheIsDirty)
+            SaveChangesAsync().Wait();
+    }
+
+    private async Task SaveChangesAsync()
+    {
+        var csvLines = Cache.Select(entity => Converter.EntityToCsv(entity));
+        await File.WriteAllLinesAsync(Filepath, csvLines);
+        CacheIsDirty = false;
     }
 }
