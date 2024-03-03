45b8bfcd132f48184f5e70606a1d0861b588ce9d
Izzat Alsharif <work@izzatalsharif.com>
2024-03-03 15:16:27 +0200

    Document `CsvToEntity` to explain what exceptions can implementers throw

--- Domain/Contract/ICsvEntityConverter.cs
+++ Domain/Contract/ICsvEntityConverter.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace AirportTicketBookingSystem.Domain.Contract;
 
 public interface ICsvEntityConverter<T>
 {
+    /// <exception cref="FormatException">Thrown when unable to parse any of the csv fields.</exception>
+    /// <exception cref="ValidationException">Thrown when any of the parsed fields do not satisfy validation attributes.</exception>
     public T CsvToEntity(string csvLine);
+
     public string EntityToCsv(T entity);
 }
