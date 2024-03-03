411813c70edd321fb769e9e8c4775b1bef6ab04a
Izzat Alsharif <work@izzatalsharif.com>
2024-03-03 21:19:49 +0200

    Add `.env` mechanism for environment variables

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using AirportTicketBookingSystem.Presentation;
 
+LoadEnvVariables();
+
 Console.WriteLine("Do You Want to Enter Manager Mode ? (y/n)");
 if (Console.ReadLine() == "y")
 {
@@ -8,4 +10,35 @@ if (Console.ReadLine() == "y")
 else
 {
     new ClientController().Start();
+}
+
+return;
+
+void LoadEnvVariables()
+{
+    var current = new DirectoryInfo(Directory.GetCurrentDirectory());
+    while (current != null && current.GetFiles("*.csproj").Length == 0)
+        current = current.Parent;
+
+    if (current == null)
+    {
+        Console.WriteLine("Project root could not be found.");
+        return;
+    }
+
+    var filepath = Path.Combine(current.FullName, ".env");
+    Console.WriteLine($"Looking for .env at: {filepath}");
+
+    if (!File.Exists(filepath))
+    {
+        Console.WriteLine(".env file not found.");
+        return;
+    }
+
+    foreach (var line in File.ReadAllLines(filepath))
+    {
+        var parts = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
+        if (parts.Length != 2) continue;
+        Environment.SetEnvironmentVariable(parts[0], parts[1]);
+    }
 }
