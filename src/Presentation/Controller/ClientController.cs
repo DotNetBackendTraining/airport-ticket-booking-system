fc4f517cfc4150c36c569085b62ffc7ae54790f2
Izzat Alsharif <work@izzatalsharif.com>
2024-03-05 15:22:08 +0200

    Add action menus for manager and client controllers

--- src/Presentation/Controller/ClientController.cs
+++ src/Presentation/Controller/ClientController.cs
@@ -1,3 +1,5 @@
+using AirportTicketBookingSystem.Presentation.Utility;
+
 namespace AirportTicketBookingSystem.Presentation.Controller;
 
 public class ClientController(IServiceProvider serviceProvider)
@@ -5,5 +7,20 @@ public class ClientController(IServiceProvider serviceProvider)
 {
     public void Start()
     {
+        PromptMenu.ActionMenu("Welcome to the Airport Ticket Booking System!", [
+            ("Search for Flights", SearchFlights),
+            ("Book a Flight", BookFlight),
+            ("Manage Bookings", ManageBookings)
+        ]);
+    }
+
+    private void BookFlight()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ManageBookings()
+    {
+        throw new NotImplementedException();
     }
 }
--- src/Presentation/Controller/ManagerController.cs
+++ src/Presentation/Controller/ManagerController.cs
@@ -1,3 +1,5 @@
+using AirportTicketBookingSystem.Presentation.Utility;
+
 namespace AirportTicketBookingSystem.Presentation.Controller;
 
 public class ManagerController(IServiceProvider serviceProvider)
@@ -5,5 +7,26 @@ public class ManagerController(IServiceProvider serviceProvider)
 {
     public void Start()
     {
+        PromptMenu.ActionMenu("Welcome to the Airport Ticket Booking System!", [
+            ("Search for Bookings", SearchBookings),
+            ("Search for Flights", SearchFlights),
+            ("Import Flights from a File", ImportFlights),
+            ("Print Validation Constraints", PrintValidations)
+        ]);
+    }
+
+    private void SearchBookings()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void ImportFlights()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PrintValidations()
+    {
+        throw new NotImplementedException();
     }
 }
