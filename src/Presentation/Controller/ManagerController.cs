24b6d46241d8a666b05685f089c4a34c2c777852
Izzat Alsharif <work@izzatalsharif.com>
2024-03-05 16:44:17 +0200

    Implement `PrintValidations` option in manager controller

--- src/Presentation/Controller/ManagerController.cs
+++ src/Presentation/Controller/ManagerController.cs
@@ -1,10 +1,14 @@
+using AirportTicketBookingSystem.Application.Contract;
 using AirportTicketBookingSystem.Presentation.Utility;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AirportTicketBookingSystem.Presentation.Controller;
 
 public class ManagerController(IServiceProvider serviceProvider)
     : GlobalController(serviceProvider)
 {
+    private IManagerService ManagerService => Provider.GetRequiredService<IManagerService>();
+
     public void Start()
     {
         PromptMenu.ActionMenu("Welcome to the Airport Ticket Booking System!", [
@@ -27,6 +31,14 @@ public class ManagerController(IServiceProvider serviceProvider)
 
     private void PrintValidations()
     {
-        throw new NotImplementedException();
+        var types = ManagerService.GetDomainEntities();
+        var menuOptions = types.Select(type =>
+        {
+            return (type.Name, (Action)PrintReport);
+
+            void PrintReport() => Console.WriteLine(ManagerService.ReportConstraints(type));
+        });
+
+        PromptMenu.ActionMenu("Available Models in the System", menuOptions.ToList(), "Go Back");
     }
 }
