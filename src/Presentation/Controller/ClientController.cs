a0e512247fdcb59cb9faba5ec18a364d8c5ba907
Izzat Alsharif <work@izzatalsharif.com>
2024-03-05 21:45:32 +0200

    Implement booking management options for client

--- src/Presentation/Controller/ClientController.cs
+++ src/Presentation/Controller/ClientController.cs
@@ -55,6 +55,55 @@ public class ClientController(IServiceProvider serviceProvider)
 
     private void ManageBookings()
     {
+        PromptMenu.ActionMenu("Bookings Actions", [
+            ("Show All Bookings", ShowBookings),
+            ("Create Booking", BookFlight),
+            ("Modify Booking", ModifyBooking),
+            ("Cancel Booking", CancelBooking)
+        ], "Go Back");
+    }
+
+    private void ShowBookings() =>
         Display.SearchResult(ClientService.GetAllBookings(PassengerId));
+
+    private void ModifyBooking()
+    {
+        var booking = FindBooking();
+        if (booking == null) return;
+
+        Console.WriteLine("Let's choose a new flight class.");
+        var flightClass = PromptDomain.FlightClass();
+        if (flightClass == null)
+        {
+            Console.WriteLine("Nothing happened.");
+            return;
+        }
+
+        booking = Booking.Create(booking.FlightId, booking.PassengerId, flightClass.Value);
+        Display.OperationResult(ClientService.UpdateBooking(booking));
+    }
+
+    private void CancelBooking()
+    {
+        var booking = FindBooking();
+        if (booking == null) return;
+
+        var res = PromptHelper.PromptYesNo("Are you sure you want to delete this booking (y/n) ?  ");
+        if (res) Display.OperationResult(ClientService.CancelBooking(booking));
+        else Console.WriteLine("Nothing happened.");
+    }
+
+    private Booking? FindBooking()
+    {
+        Console.WriteLine("Which flight booking do you want to choose ?");
+        var res = PromptHelper.TryPromptForInput("Please Enter The Flight ID:  ", int.Parse, out var flightId);
+        if (!res) return null;
+
+        var list = ClientService.GetAllBookings(PassengerId).Items
+            .Where(b => b.FlightId == flightId).Take(1).ToList();
+        if (list.Count > 0) return list[0];
+
+        Console.WriteLine("Flight with this ID was not found in your bookings!");
+        return null;
     }
 }
