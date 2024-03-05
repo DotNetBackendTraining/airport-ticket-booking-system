5a67dd950880853e2e4addbc67843467123ed247
Izzat Alsharif <work@izzatalsharif.com>
2024-03-05 16:19:37 +0200

    Introduce `ReflectionService` for reflection related tasks

---
+++ src/Application/Contract/IReflectionService.cs
@@ -0,0 +1,21 @@
+namespace AirportTicketBookingSystem.Application.Contract;
+
+/// <summary>
+/// Provides services for reflecting over types and properties within the application.
+/// </summary>
+public interface IReflectionService
+{
+    /// <summary>
+    /// Generates a report listing properties and their attributes for a given type.
+    /// </summary>
+    /// <param name="type">The type to reflect over and report on.</param>
+    /// <returns>A formatted string report detailing each property of the specified type and its associated attributes.</returns>
+    public string ReportPropertiesWithAttributes(Type type);
+
+    /// <summary>
+    /// Retrieves all class types within a specified namespace.
+    /// </summary>
+    /// <param name="namespaceFilter">The namespace to search for class types.</param>
+    /// <returns>An enumerable of <see cref="Type"/> objects representing each class found within the specified namespace.</returns>
+    public IEnumerable<Type> GetClassTypesInNamespace(string namespaceFilter);
+}
---
+++ src/Application/Service/ReflectionService.cs
@@ -0,0 +1,48 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text;
+using AirportTicketBookingSystem.Application.Contract;
+
+namespace AirportTicketBookingSystem.Application.Service;
+
+public class ReflectionService : IReflectionService
+{
+    public string ReportPropertiesWithAttributes(Type type)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine($"Properties and Attributes for model: {type.Name}");
+        sb.AppendLine(new string('-', 40));
+
+        foreach (var property in type.GetProperties())
+        {
+            sb.AppendLine($"Property: {property.Name}");
+            sb.AppendLine($"Type: {property.PropertyType.Name}");
+
+            var attributes = property.GetCustomAttributes();
+            foreach (var attribute in attributes)
+            {
+                if (attribute is RequiredAttribute)
+                {
+                    sb.AppendLine(" - Attribute: Required");
+                }
+                else
+                {
+                    sb.AppendLine($" - Attribute: {attribute.GetType().Name}");
+                }
+            }
+
+            sb.AppendLine(new string('-', 40));
+        }
+
+        return sb.ToString();
+    }
+
+    public IEnumerable<Type> GetClassTypesInNamespace(string namespaceFilter)
+    {
+        return Assembly
+            .GetExecutingAssembly()
+            .GetTypes()
+            .Where(type => type is { IsClass: true, IsAbstract: false, Namespace: not null } &&
+                           type.Namespace.StartsWith(namespaceFilter, StringComparison.Ordinal));
+    }
+}
