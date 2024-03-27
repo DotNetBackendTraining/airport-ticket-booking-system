0a97b3e9982141082ff6507e0d796e7408c81b5c
Izzat Alsharif <work@izzatalsharif.com>
2024-03-27 21:13:49 +0200

    Use dotenv.net to fetch environment variables

--- AirportTicketBookingSystem/src/Presentation/Program.cs
+++ AirportTicketBookingSystem/src/Presentation/Program.cs
@@ -1,8 +1,9 @@
 ﻿using AirportTicketBookingSystem.Presentation;
 using AirportTicketBookingSystem.Presentation.Controller;
 using AirportTicketBookingSystem.Presentation.Utility;
+using dotenv.net;
 
-LoadEnvVariables();
+DotEnv.Load();
 
 if (PromptHelper.PromptYesNo("Do You Want to Enter Manager Mode (y/n):  "))
 {
@@ -13,35 +14,4 @@ else
 {
     var provider = DependencyInjector.CreateClientServiceProvider();
     new ClientController(provider).Start();
-}
-
-return;
-
-void LoadEnvVariables()
-{
-    var current = new DirectoryInfo(Directory.GetCurrentDirectory());
-    while (current != null && current.GetFiles("*.csproj").Length == 0)
-        current = current.Parent;
-
-    if (current == null)
-    {
-        Console.WriteLine("Project root could not be found.");
-        return;
-    }
-
-    var filepath = Path.Combine(current.FullName, ".env");
-    Console.WriteLine($"Looking for .env at: {filepath}");
-
-    if (!File.Exists(filepath))
-    {
-        Console.WriteLine(".env file not found.");
-        return;
-    }
-
-    foreach (var line in File.ReadAllLines(filepath))
-    {
-        var parts = line.Split('=', StringSplitOptions.RemoveEmptyEntries);
-        if (parts.Length != 2) continue;
-        Environment.SetEnvironmentVariable(parts[0], parts[1]);
-    }
 }
