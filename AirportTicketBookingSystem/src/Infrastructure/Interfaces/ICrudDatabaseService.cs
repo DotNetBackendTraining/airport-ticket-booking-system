11e6f1e48b47f87260d0defab9e17e4270ec9ffa
Izzat Alsharif <work@izzatalsharif.com>
2024-03-28 18:46:39 +0200

    Extract two interfaces from IDatabaseService

---
+++ AirportTicketBookingSystem/src/Infrastructure/Interfaces/ICrudDatabaseService.cs
@@ -0,0 +1,45 @@
+using AirportTicketBookingSystem.Domain.Common;
+using AirportTicketBookingSystem.Domain.Interfaces;
+
+namespace AirportTicketBookingSystem.Infrastructure.Interfaces;
+
+/// <summary>
+/// Defines the basic CRUD methods for managing entities of type <typeparamref name="TEntity"/>.
+/// </summary>
+/// <typeparam name="TEntity">Type of domain entity.</typeparam>
+public interface ICrudDatabaseService<in TEntity>
+    where TEntity : IEntity
+{
+    /// <summary>
+    /// Adds a single entity of type <typeparamref name="TEntity"/> to the database.
+    /// </summary>
+    /// <param name="entity">The entity to be added.</param>
+    /// <returns>A task representing the add operation.</returns>
+    /// <exception cref="DatabaseOperationException">Thrown when an entity already exists in the database.
+    /// Or when entity is invalid (against its own attributes).</exception>
+    Task Add(TEntity entity);
+
+    /// <summary>
+    /// Updates an existing entity with a new entity in the database.
+    /// Finds the old entity by comparing the new entity with `Object.Equals()` method.
+    /// </summary>
+    /// <param name="newEntity">The new entity to replace the old entity.</param>
+    /// <exception cref="DatabaseOperationException">Thrown when the old entity does not exist.
+    /// Or when entity is invalid (against its own attributes).</exception>
+    /// <returns>A task representing the update operation.</returns>
+    /// <remarks>
+    /// Before updating an entity, ensure that all dependent entities have been appropriately managed to avoid violating relational constraints.
+    /// </remarks>
+    Task Update(TEntity newEntity);
+
+    /// <summary>
+    /// Deletes an entity of type <typeparamref name="TEntity"/> from the database.
+    /// </summary>
+    /// <param name="entity">The entity to be removed.</param>
+    /// <exception cref="DatabaseOperationException">Thrown when the entity does not exist.</exception>
+    /// <returns>A task representing the delete operation.</returns>
+    /// <remarks>
+    /// Before deleting an entity, ensure that all dependent entities have been appropriately managed to avoid violating relational constraints.
+    /// </remarks>
+    Task Delete(TEntity entity);
+}
--- AirportTicketBookingSystem/src/Infrastructure/Interfaces/IQueryDatabaseService.cs
+++ AirportTicketBookingSystem/src/Infrastructure/Interfaces/IQueryDatabaseService.cs
@@ -3,10 +3,11 @@ using AirportTicketBookingSystem.Domain.Interfaces;
 namespace AirportTicketBookingSystem.Infrastructure.Interfaces;
 
 /// <summary>
-/// Defines the basic database methods for managing entities of type <typeparamref name="TEntity"/>.
+/// Defines the basic query methods for entities of type <typeparamref name="TEntity"/>.
 /// </summary>
 /// <typeparam name="TEntity">Type of domain entity.</typeparam>
-public interface IDatabaseService<TEntity> : IQueryDatabaseService<TEntity>, ICrudDatabaseService<TEntity>
+public interface IQueryDatabaseService<out TEntity>
     where TEntity : IEntity
 {
+    IEnumerable<TEntity> GetAll();
 }
